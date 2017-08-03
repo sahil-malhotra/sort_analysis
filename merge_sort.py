02c823c87b8ed04290de6994bbd41b71ac7424fe
sahil-malhotra <thesahilmalhotra@gmail.com>
2017-08-03 13:12:25 +0530

    updated files

--- merge_sort.py
+++ merge_sort.py
@@ -2,15 +2,38 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-def insertion(list):
-    for index in range(1,len(list)):
-        value = list[index]
-        i = index - 1
-        while i>=0 and (value < list[i]):
-            list[i+1] = list[i]
-            list[i] = value
-            i = i - 1
-    print(list)
+
+def merge(left, right):
+    """Merge sort merging function."""
+
+    left_index, right_index = 0, 0
+    result = []
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+
+def merge_sort(array):
+    """Merge sort algorithm implementation."""
+
+    if len(array) <= 1:  # base case
+        return array
+
+    # divide array in half and merge sort recursively
+    half = len(array) // 2
+    left = merge_sort(array[:half])
+    right = merge_sort(array[half:])
+
+    return merge(left, right)
+
 
 def mean(a):
     return sum(a) / len(a)
@@ -21,14 +44,14 @@ def test():
     find_all_list_y = []
 
     random_lists = []
-    range_num = 500
+    range_num = 2000
 
     # creating 15 random lists with varying length like 1000, 2000.., till 15000.
     for one_list in range(15):
         random_lists.append([])
         for x in range(range_num):
             random_lists[one_list].append(random.randint(1, 1001))
-        range_num += 500
+        range_num += 2000
         graph_list_x.append(len(random_lists[one_list]))
 
         print(random_lists[one_list])
@@ -38,7 +61,7 @@ def test():
         graph_list_y = []
         for one_item in range(15):
             start = time.clock()
-            insertion(random_lists[one_item])
+            merge_sort(random_lists[one_item])
             elapsed = (time.clock() - start)
 
             graph_list_y.append(elapsed)
